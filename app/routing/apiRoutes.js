443cf80b16deb4551109dbd118cad94ee21c0347
Ben <b.wayne@me.com>
2018-04-25 11:11:45 -0600

    working. needs styling

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,13 +7,28 @@ var path = require("path");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
+    for (var i = 0; i < req.body.score.length;i++) {
+      req.body.score[i] = parseInt(req.body.score[i]);
+    }
+    var match = [];
+    for (var i = 0; i < friends.length; i++) {
+      var td = 0;
+      for (var j = 0; j < friends[i].score.length; j++) {
+        td += Math.abs(friends[i].score[j] - req.body.score[j])
+      }
+      if (td < match[0] || match.length === 0) {
+        match = [];
+        match = [td, i];
+      }
+    }
+    
     friends.push(req.body);
-    res.json(friends);
+    res.send(friends[match[1]]);
   });
 }
